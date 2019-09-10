50c2f3dc6e55405ff24fa1184e512b7fa9ab6cb2
tobiasjeckel <tobiasjeckel@gmail.com>
2019-09-10 10:14:27 +0200

    testing boilerplate

---
+++ src/bioeditor.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "./axios";
+import { render, fireEvent } from "@testing-library/react";
+
+test("When no bio is passed to it, an 'add' button is rendered", () => {
+    const { container } = render(
+        <div>
+            <h2>Tell us something about yourself 😀</h2>
+            <button onClick={this.editMode}>Add Bio</button>
+        </div>
+    );
+    expect(container.querySelector("button").innerText.toBe("Add Bio"));
+});
